c6b2221b5ec25aeafcd8a159e97105877566dc5f
hislleneloizedsa <hisllen.andrade@escola.pr.gov.br>
2026-08-21 10:56:36 -0300

    ok

---
+++ script.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", () =>
+// Seleciona o botão de curtida e o ícone de coração
+const likeBtn = document.querySelector(".left-actions.action-btn:first-child");
+if (!likeBtn) return;
+
+const likeSvg = likeBtn.querySelector("svg");
+
+// contador
+let textNode = Array.from(likeBtn.childNodes).find(
+(node) => node.nodeType === Node.TEXT_NODE && node.textContent.trim() !==
+);
+
+// Zerando o contador inicial.
+let count = 0;
+
+// Atualiza
+if (textNode) {
+  textNode.textContent = `0` ;
+
+
+
+}
